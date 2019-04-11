7d4943b33356feb8ef8514cec26480f9445b24e4
qusaikhaled <qusai952198@gmail.com>
2019-04-11 19:37:57 +0300

    create website

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    'use strict';
+    
+//      $('.navbar-nav li').on('click', function () {
+//        $(this).toggleClass('active').siblings().removeClass('active');
+//    });
+    
+    
+//    $('.food-list').height($(window).height() - 300);
+    
+});
